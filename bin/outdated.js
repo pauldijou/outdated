7ad975c5a60db5b2b8164afe17929ddd0081aeb1
Paul Dijou <paul.dijou@gmail.com>
2015-07-26 15:25:57 +0200

    Improve auth

--- bin/outdated.js
+++ bin/outdated.js
@@ -5,6 +5,37 @@
 var yargs = require('yargs');
 var chalk = require('chalk');
 var logSymbols = require('log-symbols');
+var omelette = require('omelette');
+var auth = require('../lib/auth.js');
+var config = require('../lib/config.js');
+
+// Enable completion
+var complete = omelette('outdated <command> <sub>');
+
+complete.on('command', function () {
+  this.reply(['auth', 'config']);
+});
+
+complete.on('sub', function (command) {
+  switch (command) {
+    case 'auth':
+      this.reply(['add', 'list', 'remove']);
+      break;
+    case 'config':
+      this.reply(['reset']);
+      break;
+    default:
+      this.reply([]);
+  };
+});
+
+complete.init();
+
+if (process.argv.length === 3 && process.argv[2] === '--setup') {
+  console.log('Completion installed for ' + chalk.cyan('outdated') + ' command. Open a new shell to see it in action.');
+  complete.setupShellInitFile();
+  return;
+}
 
 var legend = chalk.cyan('Legend') + ':\n'
   + logSymbols.success + '  All good, nothing to do (hidden by default)\n'
@@ -30,10 +61,12 @@ var argv = yargs
     return require('../package').version;
   })
   .alias('v', 'version')
-  .usage('\nUsage: ' + chalk.cyan('outdated') + '[command] [options]\n\n' + legend + '\n\n' + glossary + '\n\n' + warning)
+  .usage('\nUsage: ' + chalk.cyan('outdated') + ' [command] [options]\n\n' + legend + '\n\n' + glossary + '\n\n' + warning)
   .command('auth add', 'Create a new token for a specific provider')
   .command('auth list', 'Display all available authorizations')
   .command('auth remove', 'Allow you to remove one or more existing authorizations')
+  .command('config', 'Display the locally stored configuration')
+  .command('config reset', 'Removed all local files storing the configuration')
   .example('$0', 'Display all outdated packages and ask you if you want to update them.')
   .example('$0 -a', 'Display all packages and ask you if you want to update them.')
   .example('$0 -a --no-ask', 'Display all packages.')
@@ -91,21 +124,21 @@ if (argv.help) {
   console.log(yargs.help());
 } else if (argv._[0] === 'auth') {
   if (argv._[1] === 'add') {
-    require('../lib/auth.js').add();
-  } else if (argv._[1] === 'list') {
-    require('../lib/auth.js').list().catch(function (err) {
-      if (err.message) {
-        console.log(logSymbols.error + chalk.red(' [Error] ') + err.message);
-      }
-    });
+    auth.add();
+  } else if (argv._[1] === 'list' || argv._[1] === 'ls') {
+    auth.list();
   } else if (argv._[1] === 'remove') {
-    require('../lib/auth.js').remove().catch(function (err) {
-      if (err.message) {
-        console.log(logSymbols.error + chalk.red(' [Error] ') + err.message);
-      }
-    });
+    auth.remove();
   } else {
-    console.log(logSymbols.error + chalk.red(' [Error]') + ' You can only use [add] or [remove] with the [auth] command.')
+    console.log(logSymbols.error + chalk.red(' [Error]') + ' You can only use [add], [list] or [remove] with the [auth] command.')
+  }
+} else if (argv._[0] === 'config') {
+  if (argv._[1] === undefined) {
+    config.display();
+  } else if (argv._[1] === 'reset') {
+    config.reset();
+  } else {
+    console.log(logSymbols.error + chalk.red(' [Error]') + ' You can only use [reset] or nothing with the [config] command.')
   }
 } else {
   require('../index')({
