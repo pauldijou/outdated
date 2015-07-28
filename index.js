9eafa1157e08824065bd6c25b47ff7b74dbc45f7
Paul Dijou <paul.dijou@gmail.com>
2015-07-28 15:08:58 +0200

    Support package options and unprefixed jspm

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var _ = require('lodash');
 var chalk = require('chalk');
+var path = require('path');
 var textTable = require('text-table');
 var stringLength = require('string-length');
 var logSymbols = require('log-symbols');
@@ -28,13 +29,54 @@ var defaults = {
   prune: false,
   update: false,
   jsonUpdate: false,
-  verbose: false
+  verbose: 0
 };
+
+// Enable or disable managers based on options
+function setupManagers() {
+  context.verbose(1, '# Setup managers');
+
+  var hasTrue = false;
+  var hasFalse = false;
+
+  _.forEach(managers, function (m, name) {
+    if (context.options[name] === true) {
+      hasTrue = true;
+    }
+    if (context.options[name] === false) {
+      hasFalse = true;
+    }
+  });
+
+  var keptManagers = {};
+
+  if (hasTrue) {
+    // If hasTrue, only true managers are kept
+    _.forEach(managers, function (m, name) {
+      if (context.options[name] === true) {
+        keptManagers[name] = m;
+      }
+    });
+  } else if (hasFalse) {
+    // If hasFalse but not hasTrue, only non-false managers are kept
+    _.forEach(managers, function (m, name) {
+      if (context.options[name] !== false) {
+        keptManagers[name] = m;
+      }
+    });
+  } else {
+    // All undefined, keep all managers
+    keptManagers = managers;
+  }
+
+  context.verbose(1, 'Managers: ' + _.map(keptManagers, function (m) { return m.name; }));
+  context.managers = keptManagers;
+}
 
 // Perform the verification of all packages for all package manangers
 function check() {
   context.verbose(1, '# Check');
-  return Promise.all(_.map(managers, function (manager, name) {
+  return Promise.all(_.map(context.managers, function (manager, name) {
     return manager.check().then(function (dependencies) {
       context.setDependencies(name, dependencies);
       return dependencies;
@@ -49,6 +91,27 @@ function check() {
       }
     });
   }));
+}
+
+// There is a problem in case of an unprefixed use of jspm
+// Both jspm and NPM are using the same package.json file
+// With the same dependencies and devDependencies keys
+function fixUnprefixedJspm() {
+  context.verbose(1, '# Fix unprefixed jspm');
+  var packageJson = {};
+
+  try {
+    packageJson = require(path.resolve(process.cwd(), 'package.json'));
+  } catch (err) {}
+
+  if (packageJson.jspm === true && context.dependencies.npm) {
+    // This is an unprefixed jspm
+    // We need to remove all NPM dependencies since they are wrong
+    // except for the extraneous ones
+    context.setDependencies('npm', context.dependencies.npm.filter(function (dep) {
+      return dep.is.extraneous;
+    }));
+  }
 }
 
 // Output in the console the result of all the previous checks
@@ -185,29 +248,42 @@ function ask() {
 // Perform all actions depending on options and user answers
 function act() {
   context.verbose(1, '# Act');
+  context.log('');
   return Promise.all(_.map(context.answers, function (answers, name) {
     var manager = managers[name];
     var dependencies = context.dependencies[name];
     var result = Promise.resolve();
 
     if ((context.options.prune || answers.prune) && Dependency.hasAction('prune', dependencies)) {
-      result = result.then(manager.prune);
+      context.info('[' + manager.name + '] Start pruning');
+      result = result.then(manager.prune).then(function () {
+        context.info('[' + manager.name + '] Done pruning');
+      });
     }
 
     if (context.options.jsonUpdate && Dependency.hasAction('jsonUpdate', dependencies)) {
       result = result.then(function () {
+        context.info('[' + manager.name + '] Start updating');
         return manager.jsonUpdate(dependencies.filter(function (dep) {
           return dep.actions.jsonUpdate;
-        }));
+        })).then(function () {
+          context.info('[' + manager.name + '] Done updating');
+        });
       });
     } else if (answers.jsonUpdate && answers.jsonUpdate.length > 0) {
+      context.info('[' + manager.name + '] Start updating');
       result = result.then(function () {
-        return manager.jsonUpdate(answers.jsonUpdate);
+        return manager.jsonUpdate(answers.jsonUpdate).then(function () {
+          context.info('[' + manager.name + '] Done updating');
+        });
       });
     } else if ((context.options.update || answers.update) && Dependency.hasAction('update', dependencies)) {
       // That's an "else if" because "jsonUpdate" will also do an "update" anyway
+      context.info('[' + manager.name + '] Start updating');
       result = result.then(function () {
         return manager.update();
+      }).then(function () {
+        context.info('[' + manager.name + '] Done updating');
       });
     }
 
@@ -224,7 +300,6 @@ function logError(err) {
   context.log(prefix + 'Bug tracker at: https://github.com/pauldijou/outdated/issues');
   context.log('');
   context.stack(err);
-  throw err;
 }
 
 function goodbye() {
@@ -240,6 +315,9 @@ function goodbye() {
     context.log('');
   }
 
+  context.log(logSymbols.success + ' All done! Thanks for using ' + chalk.cyan('outdated') + '. See you soon.');
+  context.log('');
+
   return context;
 }
 
@@ -248,13 +326,15 @@ function outdated(opts) {
   context.setOptions(opts, defaults)
 
   context.verbose(1, '# Outdated');
-  context.verbose(2, opts);
+  context.verbose(1, 'Options: ' + JSON.stringify(opts));
 
   context.log('');
   context.info('Loading dependencies... (this might take some time)');
 
   return Promise.resolve()
+    .then(setupManagers)
     .then(check)
+    .then(fixUnprefixedJspm)
     .then(output)
     .then(ask)
     .then(act)
