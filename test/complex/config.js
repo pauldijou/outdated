b9d8172a42e33c40507beea4d4d1f4402589e133
Paul Dijou <paul.dijou@gmail.com>
2015-07-28 19:31:20 +0200

    Fix config file

--- test/complex/config.js
+++ test/complex/config.js
@@ -9,7 +9,8 @@ System.config({
 
 System.config({
   "map": {
-    "outdated-test": "npm:outdated-test@1.0.1",
-    "fake-but-awesome": "npm:fake-but-awesome@1.0.0"
+    "fake-but-awesome": "npm:fake-but-awesome@1.0.0",
+    "outdated-test": "npm:outdated-test@1.0.1"
   }
 });
+
