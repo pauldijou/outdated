a50208ad37901d7fe4f7429c3c622f4dd2be7b66
Paul Dijou <paul.dijou@gmail.com>
2015-07-31 14:02:48 +0200

    Fix test script

--- scripts/test.js
+++ scripts/test.js
@@ -8,13 +8,17 @@ var cp = require('child_process');
 var argv = require('yargs').argv;
 var utils = require('../test/utils.js');
 
+function getTests() {
+  return fs.readdirSync(path.resolve(__dirname, '..', 'test')).filter(function (filename) {
+    return filename.indexOf('.') < 0;
+  });
+}
+
 if (argv._[0] === 'reset') {
   var folders = [];
 
   if (argv._.length === 1) {
-    folders = fs.readdirSync(path.resolve(__dirname, '..', 'test')).filter(function (filename) {
-      return filename.indexOf('.') < 0;
-    });
+    folders = getTests();
   } else {
     folders = argv._.slice(1);
   }
@@ -25,8 +29,12 @@ if (argv._[0] === 'reset') {
 } else {
   var command = './node_modules/mocha/bin/mocha --recursive --no-timeouts --reporter mocha-better-spec-reporter';
 
+  if (argv._.length === 0) {
+    argv._ = getTests();
+  }
+
   command = argv._.reduce(function (c, t) {
-    return c + ' ' + path.resolve(__dirname, '..', 'test', t, 'test*.js');
+    return c + ' ' + path.resolve(__dirname, '..', 'test', t, 'test.js');
   }, command);
 
   cp.execSync(command, {stdio: 'inherit'});
